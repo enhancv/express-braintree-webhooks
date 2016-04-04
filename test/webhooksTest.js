24b44d18e66aa5bedccb7d657475525d09b3e797
Ivan Kerin <ikerin@gmail.com>
2016-04-05 00:05:36 +0300

    Clean up test

--- test/webhooksTest.js
+++ test/webhooksTest.js
@@ -14,21 +14,24 @@ const sample = function sample(kind, id) {
 const webhooks = require('../src/webhooks');
 
 describe('Webhooks middleware', () => {
+    beforeEach(() => {
+        this.action = sinon.spy();
+        this.res = { send: sinon.spy() };
+        this.next = sinon.spy();
+    });
+
     it('Should respond to check webhook', () => {
+        const middleware = webhooks(gateway, { check: this.action });
         const notification = sample(braintree.WebhookNotification.Kind.Check);
-        const action = sinon.spy();
-        const res = { send: sinon.spy() };
-        const next = sinon.spy();
-        const middleware = webhooks(gateway, { check: action });
 
-        middleware({ body: notification }, res, next);
+        middleware({ body: notification }, this.res, this.next);
 
-        sinon.assert.calledOnce(action);
-        sinon.assert.calledOnce(res.send);
-        sinon.assert.notCalled(next);
+        sinon.assert.calledOnce(this.action);
+        sinon.assert.calledOnce(this.res.send);
+        sinon.assert.notCalled(this.next);
 
         assert.equal(
-            action.getCall(0).args[0].kind,
+            this.action.getCall(0).args[0].kind,
             braintree.WebhookNotification.Kind.Check
         );
     });
@@ -38,24 +41,21 @@ describe('Webhooks middleware', () => {
             braintree.WebhookNotification.Kind.SubscriptionChargedSuccessfully,
             10
         );
-        const action = sinon.spy();
-        const res = { send: sinon.spy() };
-        const next = sinon.spy();
-        const middleware = webhooks(gateway, { subscription_charged_successfully: action });
+        const middleware = webhooks(gateway, { subscription_charged_successfully: this.action });
 
-        middleware({ body: notification }, res, next);
+        middleware({ body: notification }, this.res, this.next);
 
-        sinon.assert.calledOnce(action);
-        sinon.assert.calledOnce(res.send);
-        sinon.assert.notCalled(next);
+        sinon.assert.calledOnce(this.action);
+        sinon.assert.calledOnce(this.res.send);
+        sinon.assert.notCalled(this.next);
 
         assert.equal(
-            action.getCall(0).args[0].kind,
+            this.action.getCall(0).args[0].kind,
             braintree.WebhookNotification.Kind.SubscriptionChargedSuccessfully
         );
 
         assert.equal(
-            action.getCall(0).args[0].subscription.id,
+            this.action.getCall(0).args[0].subscription.id,
             10
         );
     });
@@ -63,52 +63,45 @@ describe('Webhooks middleware', () => {
 
     it('Should respond with next no error', () => {
         const notification = sample(braintree.WebhookNotification.Kind.Check);
-        const res = { send: sinon.spy() };
-        const next = sinon.spy();
         const middleware = webhooks(gateway, {});
 
-        middleware({ body: notification }, res, next);
+        middleware({ body: notification }, this.res, this.next);
 
-        sinon.assert.notCalled(res.send);
-        sinon.assert.calledOnce(next);
+        sinon.assert.notCalled(this.res.send);
+        sinon.assert.calledOnce(this.next);
 
         assert.equal(
-            next.getCall(0).args[0].message,
+            this.next.getCall(0).args[0].message,
             'Unknown webhook'
         );
     });
 
     it('Should handle exception inside action code and pass it to next', () => {
         const notification = sample(braintree.WebhookNotification.Kind.Check);
-        const action = sinon.spy(() => {
+        this.action = sinon.spy(() => {
             throw new Error('Some test error');
         });
-        const res = { send: sinon.spy() };
-        const next = sinon.spy();
-        const middleware = webhooks(gateway, { check: action });
+        const middleware = webhooks(gateway, { check: this.action });
 
-        middleware({ body: notification }, res, next);
+        middleware({ body: notification }, this.res, this.next);
 
-        sinon.assert.calledOnce(action);
-        sinon.assert.notCalled(res.send);
-        sinon.assert.calledOnce(next);
+        sinon.assert.calledOnce(this.action);
+        sinon.assert.notCalled(this.res.send);
+        sinon.assert.calledOnce(this.next);
 
         assert.equal(
-            next.getCall(0).args[0].message,
+            this.next.getCall(0).args[0].message,
             'Some test error'
         );
     });
 
     it('Should call next on braintree error', () => {
-        const action = sinon.spy();
-        const res = { send: sinon.spy() };
-        const next = sinon.spy();
-        const middleware = webhooks(gateway, { check: action });
+        const middleware = webhooks(gateway, { check: this.action });
 
-        middleware({ body: { bt_signature: 'asd', bt_payload: 'asd' } }, res, next);
+        middleware({ body: { bt_signature: 'asd', bt_payload: 'asd' } }, this.res, this.next);
 
-        sinon.assert.notCalled(action);
-        sinon.assert.notCalled(res.send);
-        sinon.assert.calledOnce(next);
+        sinon.assert.notCalled(this.action);
+        sinon.assert.notCalled(this.res.send);
+        sinon.assert.calledOnce(this.next);
     });
 });
